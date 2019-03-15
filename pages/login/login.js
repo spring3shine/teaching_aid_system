8d5150d78b44dd73a2647e423d6b3dbcfc0b37b1
spring3shine <lichunhui912@gmail.com>
2019-03-15 10:23:35 +0800

    添加了登陆页面（初）
    添加了登陆页面，但还需完善

---
+++ pages/login/login.js
@@ -0,0 +1,123 @@
+const app = getApp()
+
+// pages/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    appUserInfo:null,
+    phone: '',
+    password: ''
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo
+        console.log(userInfo)
+        // var nickName = userInfo.nickName
+        // var avatarUrl = userInfo.avatarUrl
+        // var gender = userInfo.gender //性别 0：未知、1：男、2：女 
+        // var province = userInfo.province
+        // var city = userInfo.city
+        // var country = userInfo.country
+        that.setData({
+          appUserInfo: userInfo
+        })
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+// 获取输入账号
+  phoneInput: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+
+  // 获取输入密码
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+
+  // 登录
+  login: function () {
+    if (this.data.phone.length == 0 || this.data.password.length == 0) {
+      wx.showToast({
+        title: '用户名和密码不能为空',
+        icon: 'loading',
+        duration: 2000
+      })
+    } else {
+      // 这里修改成跳转的页面
+      
+      //如果是学生，跳转至学生操作页面，
+      //如果是老师，跳转至老师操作页面
+      wx.showToast({
+        title: '登录成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }
+  }
+})
